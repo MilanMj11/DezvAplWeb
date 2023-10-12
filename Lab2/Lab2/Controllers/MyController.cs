92c63e031d6bb51268a93c7ad29dfcdb610aa9bc
MilanMj11 <mujdar.milan@gmail.com>
2023-10-12 19:48:44 +0300

    AddedController Sample

---
+++ Lab2/Lab2/Controllers/MyController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+namespace Lab2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MyController : ControllerBase
+    {
+        [HttpDelete("delete/{id}")]
+        public IActionResult DeleteById(int id)
+        {
+            return Ok();
+        }
+
+        [HttpGet("getAllOrdered")]
+        public IActionResult GetAllOrdered()
+        {
+            return Ok();
+        }
+    }
+}
