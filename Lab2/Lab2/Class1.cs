9c72071ec80a91842646a29b07ad02639cec1955
MilanMj11 <mujdar.milan@gmail.com>
2023-10-12 18:48:45 +0300

    laborator 2 code

---
+++ Lab2/Lab2/Class1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    internal class Class1: Interface1
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public int Value1 { get; set; } = 1;
+
+        public int Value2 { get; set; } = 2;
+
+        private int Value3 { get; set; } = 2;
+
+        protected int Value4 { get; set; } = 2;
+
+
+        private protected int Value5 { get; set; } = 2;
+
+        internal int Value6 { get; set; }
+
+        protected internal int Value7 { get; set; }
+
+        public List<string> Tags { get; set; }
+
+        
+
+    }
+}
---
+++ Lab2/Lab2/Program.cs
@@ -0,0 +1,67 @@
+﻿using Lab2.Generic;
+using Newtonsoft.Json;
+using System.Text.Json.Serialization;
+
+Console.WriteLine("Hello, World!");
+
+/// Generic
+/// 
+
+GenericClass<string> genericClassWithString = new()
+{
+    Value = "abc"
+};
+
+GenericClass<int> genericClassWithInt = new()
+{
+    Value = 123
+};
+
+Console.WriteLine(genericClassWithString.Value);
+Console.WriteLine(genericClassWithInt.Value);
+
+GenericClassMultipleParams<int, string> genericMulti = new()
+{
+    Key = 456,
+    Value = "Value: 456"
+};
+
+
+string objectSerialized = JsonConvert.SerializeObject(genericMulti);
+Console.WriteLine(objectSerialized);
+
+Tuple<int, string, string> tupleExample = new Tuple<int, string, string>(1, "ABC", "value 1");
+var tupleExample2 = Tuple.Create(2, "bdc", "value 2");
+Tuple<int, string, string, int> tupleExample3 = new(3, "ddd", "value 3,8", 8); /// 8 maxim values;
+
+int sum(int a,int b,int c = 10)
+{
+    return a + b + c;
+}
+
+Console.WriteLine(sum(1, 2));
+Console.WriteLine(sum(1, 2, 3));
+
+/// ref
+
+void sumWithRef(int a,int b,ref int c)
+{
+    c = a + b;
+}
+
+int num = 2;
+sumWithRef(4, 5, ref num);
+Console.WriteLine(num);
+
+// out
+void sumWithOut(int a,int b,out int c)
+{
+    c = a + b;
+}
+
+int c;
+sumWithOut(4, 4, out c);
+Console.WriteLine(c);
+
+var objectWithNoClass = new { Title = "abc", Size = 23 };
+dynamic student = new { LastName = "Student Last Name 1", Grupa = "24" };
