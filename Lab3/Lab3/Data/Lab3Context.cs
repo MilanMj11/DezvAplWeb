a80fb6b5e29ef5840cc1ed50ff8aec3e579c8d1b
MilanMj11 <mujdar.milan@gmail.com>
2023-10-26 20:33:49 +0300

    Laboratorul 3

---
+++ Lab3/Lab3/Data/Lab3Context.cs
@@ -0,0 +1,12 @@
+﻿using Lab3.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lab3.Data
+{
+    public class Lab3Context: DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+        public Lab3Context(DbContextOptions<Lab3Context> options) : base(options) { }
+        protected override void OnModelCreating(ModelBuilder modelBuilder) { }
+    }
+}
---
+++ Lab3/Lab3/Models/Student.cs
@@ -0,0 +1,10 @@
+﻿using Lab3.Models.Base;
+
+namespace Lab3.Models
+{
+    public class Student: BaseEntity
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
